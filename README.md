e07d5207ce2ef857d03033d93dd929e07a9c6ec5
root <root@Rill.lan>
2020-06-29 11:27:35 +0700

    ok

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-echo "# brainfuck-installer"
+#use only on stb b860h or hg680p
+wget https://raw.githubusercontent.com/jakues/brainfuck-psiphon-installer/master/install.sh
+chmod +x install.sh
+./install.sh
---
+++ install.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+#############################
+#brainfuck installer
+###########################
+#for amlogic s905x
+#########################
+#openwrt tools
+#######################
+echo ""
+echo -e "\033[1;33mUpdating packages\033[1;37m"
+echo ""
+sleep 1
+yes | opkg update
+echo ""
+echo -e "\033[1;33mInstalling requirements\033[1;37m"
+echo ""
+sleep 1
+yes | opkg install screen unzip redsocks
+clear
+echo ""
+echo -e "\033[1;33mDownloading tools\033[1;37m"
+echo ""
+sleep 1
+wget https://github.com/jakues/brainfuck-psiphon-installer/releases/download/0.1/brain.zip
+echo ""
+echo -e "\033[1;33mCreating Directory\033[1;37m"
+echo ""
+sleep 1
+mkdir ~/brain
+echo ""
+echo -e "\033[1;33mExtracting tools\033[1;37m"
+echo ""
+sleep 1
+unzip brain.zip -d ~/brain
+echo ""
+echo -e "\033[1;33mcreating simple usage script\033[1;37m"
+echo ""
+sleep 1
+cat > /usr/bin/brain << EOF
+#brainfuck simple running script
+#jakues
+ start() {
+        a=$(ls /usr/sbin | grep redsocks)
+        b=$(ls /usr/sbin/ | grep screen)
+        c="/root/brain/brainfuck-psiphon-pro-go"
+        cd
+        clear
+        echo -e "\033[1;35mBrainfuck\033[1;37m \033[1;32mez Running\033[1;37m"
+        echo -e "\033[1;34m================================\033[1;37m"
+        sleep 1
+        if [ ! -z $a ] || [ ! -z $b ]; then
+        echo -e "\033[1;36mChecking Compatible Package... \033[1;32m(OK)\033[1;37m"
+        sleep 1
+        if [ -f $c ] ; then
+        echo -e "\033[1;36mChecking Psiphon Tunnel Core... \033[1;32m(OK)\033[1;37m"
+        sleep 1
+        else
+        echo -e "\033[1;31mPlease check the compatible file\033[1;37m"
+        sleep 1
+        exit
+        fi
+        fi
+        screen -d -m -L /root/brain/brainfuck-psiphon-pro-go
+        sleep 3
+        if cat screenlog.0 | grep Connected; then
+        exit 1;
+        else
+        echo -e "\033[1;33mTunnel\033[1;37m \033[1;31mConnecting\033[1;37m"
+        sleep 3
+        if cat screenlog.0 | grep Connected; then
+        exit 1;
+        fi
+        fi
+        echo -e "\033[1;33mTunnel\033[1;37m \033[1;32mConnected\033[1;37m"
+        sleep 1
+        echo -e "\033[1;34m================================\033[1;37m"
+        echo -e "\033[1;35mBrainfuck\033[1;37m \033[1;33mrunning on background\033[1;37m"
+        sleep 1
+}
+
+ stop() {
+        clear
+        cd
+        echo -e "\033[1;31mStop\033[1;37m \033[1;33mProcess\033[1;37m \033[1;35mBrainfuck\033[1;37m \033[1;33m.....\033[1;37m"
+        sleep 1
+        echo -e "\033[1;34m================================\033[1;37m"
+        echo -e "\033[1;33mKilling process ..."
+        sleep 1
+        killall screen brainfuck-psiphon-pro-go psiphon-tunnel-core redsocks > /dev/null 2>&1
+        echo -e "Remove cache ..."
+        sleep 1
+        rm /root/screenlog.0 > /dev/null 2>&1
+        rm /root/progo/redsocks.log > /dev/null 2>&1
+        echo -e "Restart Firewall ...\033[1;37m"
+        sleep 1
+        /etc/init.d/firewall restart > /dev/null 2>&1
+        sleep 1
+        echo -e "\033[1;34m================================\033[1;37m"
+        echo -e "\033[1;32mDone\033[1;37m"
+}
+
+ log() {
+        log="screenlog.0"
+        clear && cd
+        if [ -e "$log" ]; then
+        cat ~/$log
+        elif [ -f /root/brain/screenlog.0 ]; then
+        cat ~/$log
+        else
+        echo -e "\033[1;31mlog file not exist\033[1;37m"
+        fi
+}
+
+ help() {
+        echo -e "\033[1;32mbrain start\033[1;37m        : \033[1;36mstart the \033[1;35mbrainfuck\033[1;37m \033[1;36mservice\033[1;37m"
+        echo -e "\033[1;32mbrain restart\033[1;37m      : \033[1;36mrestart \033[1;35mbrainfuck\033[1;37m \033[1;37m \033[1;36mservice\033[1;37m"
+        echo -e "\033[1;32mbrain stop\033[1;37m : \033[1;36mstop \033[1;35mbrainfuck\033[1;37m \033[1;36mservice\033[1;37m"
+        echo -e "\033[1;32mbrain log\033[1;37m  : \033[1;36mshow magic\033[1;37m"
+}
+
+case $1 in
+ "start")
+ start
+;;
+ "stop")
+ stop
+;;
+ "log")
+ log
+;;
+ "restart")
+ stop
+ start
+;;
+ *)
+clear
+help
+;;
+esac
+EOF
+echo ""
+echo -e "\033[1;33mChanging Permission\033[1;37m"
+echo ""
+sleep 1
+chmod +x /usr/bin/brain
+echo ""
+echo -e "\033[1;33mRemoving file cache\033[1;37m"
+echo ""
+rm -rf ~/brain.zip
+brain
+echo -e "\033[1;33mInstalling Brainfuck Psiphon Done\033[1;37m"
+rm -rf ~/install.sh
